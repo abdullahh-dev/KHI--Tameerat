b7220492e80c45336f8a62f65efe50421464ba18
Abdullah <abdullahaha101@gmail.com>
2024-04-19 00:25:57 +0500

    added some changes to hero section

--- khi-tameerat/src/components/home/HomeHeroSec.jsx
+++ khi-tameerat/src/components/home/HomeHeroSec.jsx
@@ -12,14 +12,14 @@ function HomeHeroSec() {
           <h1 className="max-w-[800px] text-[90px] text-center font-extrabold leading-[88px]">
             We are your trusted <span className="text-[#9ED220]">Builder</span>
           </h1>
-          <p className="max-w-[780px] text-center text-[17px] tracking-wider leading-relaxed">
+          <p className="max-w-[780px] text-center text-[17px] tracking-wider mt-6 leading-relaxed">
             KHI Tameerat your trusted partner in bringing your dreams to life.
             Weather you are planning a new construction project, remodeling an
             existing space or simply need accurate cost estimation, KHI Tameraat
             have got you covered. With expertise in every aspect of the building
             process, from concept to completion, we make your vision a reality.
           </p>
-          <span className="space-x-4 font-semibold">
+          <span className="space-x-4 mt-6 font-semibold">
             <button className="bg-[#9ed220] group px-[14px] py-[8px] rounded-[4px]">
               Get in touch
               <span>
