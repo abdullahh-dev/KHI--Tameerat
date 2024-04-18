52e79490ed73ce865c956bc41339635479f43c1b
Abdullah <abdullahaha101@gmail.com>
2024-04-19 00:28:07 +0500

    added some changes to hero section

--- khi-tameerat/src/components/home/HomeHeroSec.jsx
+++ khi-tameerat/src/components/home/HomeHeroSec.jsx
@@ -6,11 +6,11 @@ function HomeHeroSec() {
       <div
         className={`w-full relative h-[85vh] bg-cover bg-[url('/src/assets/images/mainimg.jpg')]`}>
         <div className="max-w-[1440px] text-white mx-auto w-full h-full flex flex-col justify-center items-center gap-4">
-          <p className="uppercase text-center text-[18px] font-semibold tracking-[6px] text-[#9ED220]">
+          <p className="uppercase text-center text-[18px] font-semibold tracking-[6px] text-[#9dd221]">
             Building Beyond Boundaries
           </p>
           <h1 className="max-w-[800px] text-[90px] text-center font-extrabold leading-[88px]">
-            We are your trusted <span className="text-[#9ED220]">Builder</span>
+            We are your trusted <span className="text-[#9dd221]">Builder</span>
           </h1>
           <p className="max-w-[780px] text-center text-[17px] tracking-wider mt-6 leading-relaxed">
             KHI Tameerat your trusted partner in bringing your dreams to life.
@@ -20,7 +20,7 @@ function HomeHeroSec() {
             process, from concept to completion, we make your vision a reality.
           </p>
           <span className="space-x-4 mt-6 font-semibold">
-            <button className="bg-[#9ed220] group px-[14px] py-[8px] rounded-[4px]">
+            <button className="bg-[#9dd221] group px-[14px] py-[8px] rounded-[4px]">
               Get in touch
               <span>
                 <FaLongArrowAltRight className="inline ml-1 group-hover:translate-x-[2px] transition-all duration-500" />
