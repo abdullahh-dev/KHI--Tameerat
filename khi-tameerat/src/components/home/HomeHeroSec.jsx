9d202f81d26d38b5fc7da9e65cf3d8140aeb6ae2
Abdullah <abdullahaha101@gmail.com>
2024-04-19 01:12:15 +0500

    mobile optimized changes made

--- khi-tameerat/src/components/home/HomeHeroSec.jsx
+++ khi-tameerat/src/components/home/HomeHeroSec.jsx
@@ -15,17 +15,17 @@ function HomeHeroSec() {
           <p className="uppercase text-center md:text-[18px] text-[12px] font-semibold tracking-[4px] md:tracking-[6px] text-[#9dd221]">
             Building Beyond Boundaries
           </p>
-          <h1 className="max-w-[800px] text-[48px] md:text-[90px] text-center font-extrabold leading-tight md:leading-[88px]">
+          <h1 className="max-w-[800px] text-[56px] md:text-[90px] text-center font-extrabold leading-[56px] md:leading-[88px]">
             We are your trusted <span className="text-[#9dd221]">Builder</span>
           </h1>
-          <p className="max-w-[780px] text-center text-[12px] md:text-[17px] tracking-wider mt-6 leading-relaxed">
+          <p className="max-w-[780px] text-center text-[12px] md:text-[17px] tracking-wider mt-2 md:mt-6 leading-relaxed">
             KHI Tameerat your trusted partner in bringing your dreams to life.
             Weather you are planning a new construction project, remodeling an
             existing space or simply need accurate cost estimation, KHI Tameraat
             have got you covered. With expertise in every aspect of the building
             process, from concept to completion, we make your vision a reality.
           </p>
-          <span className="flex flex-col w-full justify-center md:flex-row gap-4 md:space-x-4 mt-6 font-semibold">
+          <span className="flex flex-col w-full justify-center md:flex-row gap-4 md:space-x-4 mt-3 md:mt-6 font-semibold">
             <button className="bg-[#9dd221] group px-[14px] py-[8px] rounded-[4px]">
               Get in touch
               <span>
