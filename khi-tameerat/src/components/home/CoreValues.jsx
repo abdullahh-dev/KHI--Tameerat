fb4ed3e717b2abb843816143065965db4155b1e2
Abdullah <abdullahaha101@gmail.com>
2024-04-23 00:21:06 +0500

    modification

--- khi-tameerat/src/components/home/CoreValues.jsx
+++ khi-tameerat/src/components/home/CoreValues.jsx
@@ -17,76 +17,76 @@ function CoreValues() {
       <div className="mt-[90px] bg-[#333333] w-full py-24">
         <div className="max-w-[1440px] flex justify-center items-center w-full h-full  mx-auto">
           <div className="flex justify-center gap-x-2 md:gap-x-12 gap-y-12 flex-wrap">
-            <div className="flex flex-col w-[300px]  hover:bg-[#282828] rounded-md p-2 transition-all duration-500 gap-y-2  items-center">
+            <div className="flex flex-col w-[300px]  hover:bg-[#282828] space-y-4 rounded-md p-2 transition-all duration-500 gap-y-2  items-center">
               <span className="text-[#9ED220]">
                 {' '}
                 <FaUserGraduate size={80} />{' '}
               </span>
-              <p className="text-white text-[24px] font-semibold">
+              <p className="text-white text-[18px] font-semibold">
                 Experienced Professionals
               </p>
             </div>
-            <div className="flex flex-col hover:bg-[#282828] rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
+            <div className="flex flex-col hover:bg-[#282828] space-y-4 rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
               <span className="text-[#9ED220]">
                 {' '}
                 <HiBadgeCheck size={80} />{' '}
               </span>
-              <p className="text-white text-[24px] font-semibold">
+              <p className="text-white text-[18px] font-semibold">
                 Quality Assurance
               </p>
             </div>
-            <div className="flex flex-col hover:bg-[#282828] rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
+            <div className="flex flex-col hover:bg-[#282828] space-y-4 rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
               <span className="text-[#9ED220]">
                 {' '}
                 <FaHandshake size={80} />{' '}
               </span>
-              <p className="text-white text-[24px] font-semibold">
+              <p className="text-white text-[18px] font-semibold">
                 Client Centric Approach
               </p>
             </div>
-            <div className="flex flex-col hover:bg-[#282828] rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
+            <div className="flex flex-col hover:bg-[#282828] space-y-4 rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
               <span className="text-[#9ED220]">
                 {' '}
                 <MdEco size={80} />{' '}
               </span>
-              <p className="text-white text-[24px] font-semibold">
+              <p className="text-white text-[18px] font-semibold">
                 Eco-Friendly Practices
               </p>
             </div>
 
-            <div className="flex flex-col hover:bg-[#282828] rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
+            <div className="flex flex-col hover:bg-[#282828] space-y-4 rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
               <span className="text-[#9ED220]">
                 {' '}
                 <IoIosPricetag size={80} />{' '}
               </span>
-              <p className="text-white text-[24px] font-semibold">
+              <p className="text-white text-[18px] font-semibold">
                 Budget Friendly Options
               </p>
             </div>
-            <div className="flex flex-col hover:bg-[#282828] rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
+            <div className="flex flex-col hover:bg-[#282828] space-y-4 rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
               <span className="text-[#9ED220]">
                 {' '}
                 <TbBulbFilled size={80} />{' '}
               </span>
-              <p className="text-white text-[24px] font-semibold">
+              <p className="text-white text-[18px] font-semibold">
                 Innovative Design
               </p>
             </div>
-            <div className="flex flex-col hover:bg-[#282828] rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
+            <div className="flex flex-col hover:bg-[#282828] space-y-4 rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
               <span className="text-[#9ED220]">
                 {' '}
                 <IoMdClock size={80} />{' '}
               </span>
-              <p className="text-white text-[24px] font-semibold">
+              <p className="text-white text-[18px] font-semibold">
                 Flexible Scheduling
               </p>
             </div>
-            <div className="flex flex-col hover:bg-[#282828] rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
+            <div className="flex flex-col hover:bg-[#282828] space-y-4 rounded-md p-2 transition-all duration-500 gap-y-2 w-[300px] items-center">
               <span className="text-[#9ED220]">
                 {' '}
                 <FaUserGear size={80} />{' '}
               </span>
-              <p className="text-white text-[24px] font-semibold">
+              <p className="text-white text-[18px] font-semibold">
                 Personalized Solutions
               </p>
             </div>
